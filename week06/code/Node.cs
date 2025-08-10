03310d2300b63716ae3929fecbaa274c652979a9
emmanuel1539 <ekenedonzube1539@gmail.com>
2025-08-10 03:05:49 +0100

    Week 06 completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,35 +11,43 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+    // Problem 1: Skip duplicates
+    if (value == Data)
+        return; // Do nothing if the value already exists
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
+    if (value < Data)
+    {
+        // Insert to the left
+        if (Left is null)
+            Left = new Node(value);
         else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+            Left.Insert(value);
     }
-
+    else // value > Data
+    {
+        // Insert to the right
+        if (Right is null)
+            Right = new Node(value);
+        else
+            Right.Insert(value);
+    }
+}
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
-    }
+    // Problem 2: Recursive search
+    if (value == Data)
+        return true;
 
+    if (value < Data)
+        return Left != null && Left.Contains(value);
+    else
+        return Right != null && Right.Contains(value);
+}
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
-    }
+    // Problem 4: Height = 1 + max(height of left, height of right)
+    int leftHeight = Left?.GetHeight() ?? 0;
+    int rightHeight = Right?.GetHeight() ?? 0;
+    return 1 + Math.Max(leftHeight, rightHeight);
+}
 }
